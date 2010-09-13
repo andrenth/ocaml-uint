1590e0ce4d08f9cb70bb2bbf7455c1e6c7b33fd5
gene9 <a@example.com>
2010-09-14 00:02:56 +0300

    test

--- uint32.mli
+++ uint32.mli
@@ -35,3 +35,4 @@ val printer : Format.formatter -> uint32 -> unit
 val printer_bin : Format.formatter -> uint32 -> unit
 val printer_oct : Format.formatter -> uint32 -> unit
 val printer_hex : Format.formatter -> uint32 -> unit
+
